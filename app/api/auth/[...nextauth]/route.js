9501804e6e939178b708b47a2f2b5bc4cc043327
Abdullah <mianabdullah125125@gmail.com>
2023-05-19 11:21:07 +0500

    signout/signin done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -16,31 +16,35 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
 
-    session.user.id = sessionUser._id.tostring();
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connecttoDB();
-      //check is user is already exist
-
-      const userExists = await User.findOne({ email: profile.email });
-
-      // if not create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
-      }
-    } catch (error) {}
-  },
+  callbacks:{
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+  
+      session.user.id = sessionUser._id.tostring();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connecttoDB();
+        //check is user is already exist
+  
+        const userExists = await User.findOne({ email: profile.email });
+  
+        // if not create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+      } catch (error) {}
+    },
+  }
+ 
 });
 
 export { handler as GET, handler as POST };
